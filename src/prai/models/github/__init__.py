fb604a3d3d357c8ae15371eb21bd098aa5fd5f7e
Tyler Stuessi <tyler.stuessi@gmail.com>
2025-07-29 19:13:42 -0500

    Add PullRequestGet type

--- src/prai/models/github/__init__.py
+++ src/prai/models/github/__init__.py
@@ -1,4 +1,5 @@
 from .user import Model as User
 from .user import PrivateUser, PublicUser, Plan
+from .pull_request__get import PullRequest as PullRequestGet
 
-__all__ = ["User", "PrivateUser", "PublicUser", "Plan"]
+__all__ = ["User", "PrivateUser", "PublicUser", "Plan", "PullRequestGet"]
